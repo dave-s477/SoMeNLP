5a991e6a2e2ae48c8388c8be5d6a5eb28b229250
ds626 <david.schindler@uni-rostock.de>
2021-04-20 16:59:25 +0200

    add configurations

---
+++ setup.py
@@ -0,0 +1,58 @@
+import sys
+if sys.version_info <= (3,7):
+    sys.exit('Python >= 3.7 is required')
+
+from setuptools import setup
+
+def readme():
+    with open('Readme.md') as f:
+        return f.read()
+
+setup(name='SoMeNLP',
+      version='0.1',
+      description='NLP procedures for scientific articles and software extraction.',
+      long_description=readme(),
+      classifiers=[
+        'License :: OSI Approved :: GPLv3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: NER :: NLP',
+      ],
+      keywords='scientific entity relation software paper article',
+      url='https://github.com/dave-s477/SoMeNLP',
+      author='David Schindler',
+      author_email='david.schindler@uni-rostock.de',
+      license='GPLv3',
+      packages=['somenlp'],
+      scripts=[
+        'bin/train_word_emb',
+        'bin/distant_supervision',
+        'bin/custom_feature_gen',
+        'bin/train_model',
+        'bin/tune_model',
+        'bin/tune_relext',
+        'bin/predict',
+        'bin/split_data',
+        'bin/train_relext',
+        'bin/load_dbpedia_info',
+        'bin/entity_disambiguation',
+        'bin/somesci_disambiguation_input'
+      ],
+      install_requires=[
+        'pytest',
+        'gensim==3.8.3',
+        'torch',
+        'tensorboard',
+        'pandas',
+        'numpy',
+        'beautifulsoup4',
+        'wiktextract',
+        'wget', 
+        'NLTK',
+        'scikit-learn',
+        'transformers',
+        'SPARQLWrapper',
+        'python-levenshtein',
+        'articlenizer @ https://github.com/dave-s477/articlenizer/tarball/master#egg=package'
+      ],
+      include_package_data=True,
+      zip_safe=False)
