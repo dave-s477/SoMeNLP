3194b5de8b5e05f18d27081f0a13e8177a89dd89
Bekalu <beckpostgrad@gmail.com>
2022-02-07 07:29:56 +0100

    chnaged training depth back to 3

--- somenlp/NER/models/multi_bert.py
+++ somenlp/NER/models/multi_bert.py
@@ -901,12 +901,12 @@ class BERTMultiTaskOpt2(BertPreTrainedModel):
         self.bert = BertModel(config, add_pooling_layer=False)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.software_classifier = nn.Linear(config.hidden_size, config.num_labels['software'])
-        self.mention_type_classifier = nn.Linear(config.hidden_size + config.num_labels['software'], config.num_labels['mention_type'])
-        self.soft_type_classifier = nn.Linear(config.hidden_size + config.num_labels['software'] + config.num_labels['mention_type'], config.num_labels['soft_type'])
-        self.soft_purpose_classifier = nn.Linear(config.hidden_size + config.num_labels['software'] + config.num_labels['mention_type'] + config.num_labels['soft_type'], config.num_labels['soft_purpose'])
+        
+        self.soft_type_classifier = nn.Linear(config.hidden_size + config.num_labels['software'], config.num_labels['soft_type'])
+        self.soft_purpose_classifier = nn.Linear(config.hidden_size + config.num_labels['software'] + config.num_labels['soft_type'], config.num_labels['soft_purpose'])
         self.init_weights()
 
-    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None, software_labels=None, soft_type_labels=None, mention_type_labels=None, soft_purpose_labels = None, sequence_lengths=None, output_attentions=None, output_hidden_states=None, return_dict=None, train_depth=4, teacher_forcing=False,):
+    def forward(self, input_ids=None, attention_mask=None, token_type_ids=None, position_ids=None, head_mask=None, inputs_embeds=None, software_labels=None, soft_type_labels=None, mention_type_labels=None, soft_purpose_labels = None, sequence_lengths=None, output_attentions=None, output_hidden_states=None, return_dict=None, train_depth=3, teacher_forcing=False,):
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         outputs = self.bert(
@@ -936,20 +936,10 @@ class BERTMultiTaskOpt2(BertPreTrainedModel):
         software_classified_sequence = torch.cat((sequence_output, software_labels_one_hot), dim=-1)
 
 
-        # layer -2 : classify mention_type -- identify all software_mention_types  like Usage, Creation, Deposition, Mention from the above sequence
 
-        mention_type_logits = self.mention_type_classifier(software_classified_sequence)
-        if not teacher_forcing or soft_type_labels is None:
-            mention_type_labels_one_hot = F.softmax(mention_type_logits.detach(), dim=-1)
-            mention_type_labels_one_hot = F.one_hot(torch.argmax(mention_type_labels_one_hot, axis=2), num_classes=self.num_labels['mention_type']).float()
-        else:
-            mention_type_labels_one_hot = F.one_hot(mention_type_labels, num_classes=self.num_labels['mention_type']).float()
-        mention_type_classified_sequence = torch.cat((sequence_output, software_labels_one_hot, mention_type_labels_one_hot), dim=-1)
+        # layer -2 : classify software_type -- identify all software_types  like Application. PlugIn, ProgrammingENvironment, and OperatingSystem
 
-
-        # layer -3 : classify software_type -- identify all software_types  like Application. PlugIn, ProgrammingENvironment, and OperatingSystem
-
-        soft_type_logits = self.soft_type_classifier(mention_type_classified_sequence)
+        soft_type_logits = self.soft_type_classifier(software_classified_sequence)
         if not teacher_forcing or soft_purpose_labels is None:
             soft_type_labels_one_hot = F.softmax(soft_type_logits.detach(), dim=-1)
             soft_type_labels_one_hot = F.one_hot(torch.argmax(soft_type_labels_one_hot, axis = 2), num_classes= self.num_labels['soft_type']).float()
@@ -959,7 +949,7 @@ class BERTMultiTaskOpt2(BertPreTrainedModel):
 
 
 
-        # layer -4 : classify software_purpose -- identify all software_purposes like Analysis, DataCollection, PrecProcessing, Visualization, simulation, etc.
+        # layer -3 : classify software_purpose -- identify all software_purposes like Analysis, DataCollection, PrecProcessing, Visualization, simulation, etc.
 
         soft_purpose_logits = self.soft_purpose_classifier(software_type_classified_sequence)
 
@@ -993,22 +983,8 @@ class BERTMultiTaskOpt2(BertPreTrainedModel):
             else:
                 loss += loss_fct(soft_type_logits.view(-1, self.num_labels['soft_type']), soft_type_labels.view(-1))
 
-
-        if train_depth > 1 and mention_type_labels is not None:
-            loss_fct = CrossEntropyLoss()
-            # Only keep active parts of the loss
-            if attention_mask is not None:
-                active_loss = attention_mask.view(-1) == 1
-                active_logits = mention_type_logits.view(-1, self.num_labels['mention_type'])
-                active_labels = torch.where(
-                    active_loss, mention_type_labels.view(-1), torch.tensor(loss_fct.ignore_index).type_as(mention_type_labels)
-                )
-                loss += loss_fct(active_logits, active_labels)
-            else:
-                loss += loss_fct(mention_type_logits.view(-1, self.num_labels['mention_type']), mention_type_labels.view(-1))
-        
        
-        if train_depth > 2 and soft_purpose_labels is not None:
+        if train_depth > 1 and soft_purpose_labels is not None:
             loss_fct = CrossEntropyLoss()
             # Only keep active parts of the los
             if attention_mask is not None:
@@ -1020,7 +996,7 @@ class BERTMultiTaskOpt2(BertPreTrainedModel):
                 loss += loss_fct(active_logits, active_labels)
 
 
-        return [loss, software_logits, soft_type_logits, mention_type_logits, soft_purpose_logits, outputs.hidden_states, outputs.attentions]
+        return [loss, software_logits, soft_type_logits, soft_purpose_logits, outputs.hidden_states, outputs.attentions]
 
 class BERTMultiTaskCRF(BertPreTrainedModel):
 
